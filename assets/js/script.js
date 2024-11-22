dd2204da6dbd818a70b4339a4b2a78cfcf75d80e
Gavin Jameson <m2pyeoeh@students.codeinstitute.net>
2024-11-22 15:23:56 +0000

    added division function

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,9 +34,16 @@ function runGame(gameType) {
     document.getElementById("answer-box").value = "";
     document.getElementById("answer-box").focus();
 
-    // Creates two random numbers between 1 and 25
-    let num1 = Math.floor(Math.random() * 25) + 1;
-    let num2 = Math.floor(Math.random() * 25) + 1;
+    let num1, num2;
+
+    if (gameType === "division") {
+        num2 = Math.floor(Math.random() * 25) + 1;
+        let maxMultiple = Math.floor(50 / num2);
+        num1 = num2 * (Math.floor(Math.random() * maxMultiple) + 1);
+    } else {
+        num1 = Math.floor(Math.random() * 25) + 1;
+        num2 = Math.floor(Math.random() * 25) + 1;
+    }
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
@@ -44,8 +51,9 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
-    }
-        else {
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+    } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -90,7 +98,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    }    
+    }  else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    }  
        else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -139,5 +149,13 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 
 }
